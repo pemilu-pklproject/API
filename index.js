cf4d2210680a73345ff56f09056d1d7c788a19be
Ilham Zukhri <112614524+ilhamZ354@users.noreply.github.com>
2024-02-01 11:30:24 +0700

    add express and sequlize init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+// const routes = require("./config/routes")
+const bodyParser = require("body-parser")
+// const cors = require('cors');
+const PORT = 3000;
+
+app.use(bodyParser.json())
+// app.use(cors());
+ 
+app.use(express.json());
+ 
+app.post('/', function (req, res) {
+    console.log(req.body.name)
+    res.end();
+})
+ 
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});

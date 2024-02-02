66019fec0422b7e6bcfcb6996212b98b5ccab615
Ilham Zukhri <112614524+ilhamZ354@users.noreply.github.com>
2024-02-02 14:04:03 +0700

    first buat API pemilih

--- config/routes.js
+++ config/routes.js
@@ -6,11 +6,19 @@ const {
     insertRelawan 
 } = require("../controller/relawan")
 
+const { 
+    insertPemilih 
+} = require("../controller/pemilih")
+
+
 module.exports = (app) =>{
     //relawan
     app.post('/relawan', insertRelawan);
 
     //kandidat
     app.post('/kandidat', insertKandidat)
+
+    //pemilih
+    app.post('/pemilih', insertPemilih)
 };
 

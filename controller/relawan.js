a8eae3ccc5d7b3d34a63015bedafc5f12175cbe0
Ilham Zukhri <112614524+ilhamZ354@users.noreply.github.com>
2024-02-02 09:06:06 +0700

    tes pertama buat API untuk relawan

---
+++ controller/relawan.js
@@ -0,0 +1,16 @@
+
+  const insertRelawan = async (req, res) => {
+    try {
+        res.json({
+            status: true,
+            statusCode: 200,
+            data: req.body
+        });
+        console.log("berhasil");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Failed");
+    }
+};
+
+module.exports = { insertRelawan };
